02f04d68eff7fc54ed0b8028821bb842cffa9716
hlong12042 <58381595+hlong12042@users.noreply.github.com>
2021-06-28 10:48:16 +0700

    Add files via upload

--- NhanVien.h
+++ NhanVien.h
@@ -1,12 +1,19 @@
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <conio.h>
 #include <string>
 #include <string.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <thread>
 #include "HoaDon.h"
 #define MAX_NV 500
 #define MAX_HO 32
 #define MAX_TEN 10
+#define MAX_NVTOSTRING 4
+#define DEFAULT_NVFILE "NhanVien.txt"
 using namespace std;
 //=====struct=====//
 struct NhanVien{
@@ -21,32 +28,74 @@ struct ListNhanVien{
 	NhanVien *nhanViens[MAX_NV];
 };
 //=====Ten Ham=====//
+vector<string> split(const string& str, const string& delim);
 NhanVien createNhanVien(int maNV, string ho, string ten, int phai);
-NhanVien get(ListNhanVien &list, int maNV);
-
-int indexOf(ListNhanVien &list, int maNV);
+NhanVien createNhanVien(string line);
+ListNhanVien creatListNhanVien();
+NhanVien get(const ListNhanVien &list, int maNV);
+int indexOf(const ListNhanVien &list, int maNV);
 int add(ListNhanVien &list, NhanVien &nv);
 int insertOrder(ListNhanVien &list, NhanVien &nv);
 int remove(ListNhanVien &list, int maNV);
-
 int sortmaNV(ListNhanVien &list);
 int sortTen(ListNhanVien &list);
 int compareTen(NhanVien *nv1, NhanVien *nv2);
-string toString(NhanVien &nv);
+string toString(NhanVien nv);
+int readFile(ListNhanVien &list, const string &filename);
+int writeFileAll(const ListNhanVien &list, const string &filename);
+ListNhanVien copyList(const ListNhanVien &list);
 //=====Ham=====//
-NhanVien createNhanVien(int maNV, string &ho, string &ten, int phai){
+vector<string> split(const string& str, const string& delim){
+    vector<string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == string::npos) pos = str.length();
+        string token = str.substr(prev, pos-prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    }
+    while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+NhanVien createNhanVien(int maNV, string ho, string ten, int phai){
 	NhanVien nv;
 	nv.maNV = maNV;
+	for (int i=0; i<32; i++){
+		if(i<ho.length()) ho[i] = toupper(ho[i]);
+		else ho+= " ";
+	}
 	nv.ho = ho;
+	for (int i=0; i<10; i++){
+		if(i<ten.length()) ten[i] = toupper(ten[i]);
+		else ten+= " ";
+	}
 	nv.ten = ten;
 	nv.phai = phai;
 	nv.hoaDons = new ListHoaDon;
+	return nv;
 };
-NhanVien get(ListNhanVien &list, int maNV){
+NhanVien createNhanVien(string line){
+	vector<string> v = split(line, "|");
+	NhanVien nv;
+	nv.maNV = atoi(v[0].c_str());
+	nv.ho = v[1];
+	nv.ten = v[2];
+	nv.phai = atoi(v[3].c_str());
+	nv.hoaDons = new ListHoaDon;
+	return nv;
+};
+ListNhanVien creatListNhanVien(){
+	ListNhanVien list;
+	list.n = 0;
+	return list;
+};
+NhanVien get(const ListNhanVien &list, int maNV){
 	for (int i=0; i<list.n; i++)
 		if(list.nhanViens[i]->maNV==maNV) return *list.nhanViens[i];
 };
-int indexOf(ListNhanVien &list, int maNV){
+int indexOf(const ListNhanVien &list, int maNV){
 	for (int i=0; i<list.n; i++)
 		if(list.nhanViens[i]->maNV==maNV) return i;
 	return -1;
@@ -72,7 +121,7 @@ int remove(ListNhanVien &list, int maNV){
 	int i = indexOf(list, maNV);
 	if(i==-1) return -1;
 	delete list.nhanViens[i];
-	for (int j=i; j<list.n-1; j++) list.nhanViens[i] = list.nhanViens[i+1];
+	for (int j=i+1; j<list.n; j++) list.nhanViens[j-1] = list.nhanViens[j];
 	list.n--;
 	return 1;
 };
@@ -95,24 +144,47 @@ int sortTen(ListNhanVien &list){
 	for (int i=0; i<list.n-1; i++){
 		int min = i;
 		for (int j=i+1; j<list.n; j++){
-			if(compareTen(list.nhanViens[min],list.nhanViens[j])) min=j;
+			if(compareTen(list.nhanViens[min],list.nhanViens[j])>0) min=j;
 		}
 		NhanVien* temp = list.nhanViens[i];
 		list.nhanViens[i] = list.nhanViens[min];
 		list.nhanViens[min] = temp;
 	}
 };
-string toString(NhanVien &nv){
-	string str = "";
-	if (nv.maNV/10==0) str = "\n|  " + to_string(nv.maNV);
-	else if (nv.maNV/100==0) str = "\n| " + to_string(nv.maNV);
-	else str = "\n|" + to_string(nv.maNV);
-	str+= "|";
-	str+= nv.ho; 
-	str+= "|";
-	str+= nv.ten;
-	str+= "|";
-	if (nv.phai==0) str+="nam|";
-	else str+="nu |";
+string toString(NhanVien nv){
+	string str = to_string(nv.maNV) + "|" + nv.ho + "|" + nv.ten + "|" + to_string(nv.phai) + "|";
 	return str;
+};
+int readFile(ListNhanVien &list, const string &filename){
+	FILE *f;
+	if ((f=fopen(filename.c_str(), "rb"))==NULL) {
+		fclose(f);
+		return -1;
+	}
+	int i=-1;
+	while (!feof(f))
+		fread(&list.nhanViens[++i], sizeof(NhanVien), 1, f);
+	list.n=i;
+	fclose(f);
+	return 1;
+};
+int writeFileAll(const ListNhanVien &list, const string &filename){
+	FILE *f; 
+	if ((f=fopen(filename.c_str(), "wb"))==NULL) {
+		fclose(f);
+		return -1;	
+	}
+	for (int i=0; i<list.n; i++)
+		fwrite(&list.nhanViens[i], sizeof(NhanVien),1, f);
+	fclose(f);
+	return 1;
+};
+ListNhanVien copyList(const ListNhanVien &list){
+	ListNhanVien index;
+	index.n = list.n;
+	for (int i=0; i<list.n; i++){
+		index.nhanViens[i] = new NhanVien;
+		*index.nhanViens[i] = *list.nhanViens[i];
+	}
+	return index;
 };
