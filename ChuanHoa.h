a4b1e97bda42b825ec59600558c0f0db569a80c2
hlong12042 <58381595+hlong12042@users.noreply.github.com>
2021-08-05 09:47:17 +0700

    Add files via upload

--- ChuanHoa.h
+++ ChuanHoa.h
@@ -3,12 +3,15 @@
 #include <string>
 #include <string.h>
 using namespace std;
+string ChuSo[10]{ " khong", " mot", " hai", " ba"," bon", " nam", " sau", " bay", " tam", " chin" };
+string Tien[6]{ "", " nghin", " trieu", " ty", " nghin ty", " trieu ty" };
 //=====Ten Ham=====//
 bool isNumber(char c);
 bool isAlphabet(char c);
 bool isBlackchar(char c);
 void trim(string &a);
-void chuan_hoa(string &a);
+void chuan_hoa_so(string &a);
+
 bool isNumber(char c){
 	return (48<=c&&c<=57);
 };
@@ -25,13 +28,133 @@ void trim(string &a){
 	while(a[0] == ' ') a.erase(a.begin()+0);//xoa space dau
 	while(a[a.length()-1] == ' ') a.erase(a.length()-1);//xoa space cuoi
 }
-void chuan_hoa(string &a){
+void chuan_hoa_so(string &a){
 	trim(a);
-	//xoa space giua (n space -> 1 space)
-	for(int i=0; i<a.length(); i++){
-		if(a[i]==' ' && a[i+1]==' ') {
-			a.erase(a.begin()+i+1);
-			i--;
-		}
+	while(a[0] == '0') a.erase(a.begin()+0);//xoa so 0 dau
+}
+
+
+string DocSo3ChuSo(int baso)
+{
+	int tram, chuc, donvi;
+	string KetQua = "";
+	tram = (int)(baso / 100);
+	chuc = (int)((baso % 100) / 10);
+	donvi = baso % 10;
+	if ((tram == 0) && (chuc == 0) && (donvi == 0)) return "";
+	if (tram != 0)
+	{
+		KetQua += ChuSo[tram] + " tram";
+		if ((chuc == 0) && (donvi != 0)) KetQua += " linh";
 	}
+	if ((chuc != 0) && (chuc != 1))
+	{
+		KetQua += ChuSo[chuc] + " muoi";
+		if ((chuc == 0) && (donvi != 0)) KetQua = KetQua + " linh";
+	}
+	if (chuc == 1) KetQua += " muoi";
+	switch (donvi)
+	{
+	case 1:
+	{
+		if ((chuc != 0) && (chuc != 1))
+		{
+			KetQua += " mot";
+		}
+		else
+		{
+			KetQua += ChuSo[donvi];
+		}
+		break;
+	}
+	case 5:
+	{
+		if (chuc == 0)
+		{
+			KetQua += ChuSo[donvi];
+		}
+		else
+		{
+			KetQua += " lam";
+		}
+		break;
+	}
+	default:
+	{
+		if (donvi != 0)
+		{
+			KetQua += ChuSo[donvi];
+		}
+		break;
+	}
+	}
+	return KetQua;
+}
+
+string DocTienBangChu(long SoTien, string strTail)
+{
+	int lan, i;
+	long so;
+	string KetQua = "", tmp = "";
+	int* ViTri = new int[6];
+	if (SoTien < 0) return "So am";
+	if (SoTien == 0) return "Khong " + strTail;
+	if (SoTien > 0)
+	{
+		so = SoTien;
+	}
+	else
+	{
+		so = -SoTien;
+	}
+
+	if (SoTien > 8999999999999999)
+	{
+		SoTien = 0;
+		return "";
+	}
+	ViTri[5] = (int)(so / 1000000000000000);
+	so = so - ViTri[5] * 1000000000000000;
+	ViTri[4] = (int)(so / 1000000000000);
+	so = so - ViTri[4] * +1000000000000;
+	ViTri[3] = (int)(so / 1000000000);
+	so = so - ViTri[3] * 1000000000;
+	ViTri[2] = (int)(so / 1000000);
+	ViTri[1] = (int)((so % 1000000) / 1000);
+	ViTri[0] = (int)(so % 1000);
+	if (ViTri[5] > 0)
+	{
+		lan = 5;
+	}
+	else if (ViTri[4] > 0)
+	{
+		lan = 4;
+	}
+	else if (ViTri[3] > 0)
+	{
+		lan = 3;
+	}
+	else if (ViTri[2] > 0)
+	{
+		lan = 2;
+	}
+	else if (ViTri[1] > 0)
+	{
+		lan = 1;
+	}
+	else
+	{
+		lan = 0;
+	}
+	for (i = lan; i >= 0; i--)
+	{
+		tmp = DocSo3ChuSo(ViTri[i]);
+		KetQua += tmp;
+		if (ViTri[i] != 0) KetQua += Tien[i];
+		//if ((i > 0) && (!tmp.empty())) KetQua += ",";
+	}
+	//if (KetQua.substr(KetQua.length() - 1, 1) == ",") KetQua = KetQua.substr(0, KetQua.length() - 1);
+	trim(KetQua);
+	KetQua = KetQua + " " + strTail;
+	return KetQua.substr(0, 1) + KetQua.substr(1);
 }
